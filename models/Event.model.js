7360a4c7779ac2a7549fb4d8f01c59e2af458197
Viviane Huang <vivianehuang2022@gmail.com>
2024-01-25 10:33:47 +0100

    event model updated

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,11 +1,34 @@
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require("mongoose");
 
-const eventSchema =new Schema(
-    {
-        titel:{
-            type: String,
-        }
-        description :{type :String}
+const eventSchema = new Schema(
+  {
+    titel: { type: String, required: true },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+      // The dates of the first and last episodes of
+      // Star Trek: The Next Generation
+      min: "2024-01-01",
+      max: "2030-12-27",
+    },
+    category: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    }
-)
+module.exports = Event;
