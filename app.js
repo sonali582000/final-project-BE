d8b28ec9636a031a0c2b4d83d538bac75a1c3c69
sonali582000 <rathodsonali393@gmail.com>
2024-01-25 16:28:27 +0100

    resolved merge conflict

--- app.js
+++ app.js
@@ -14,13 +14,10 @@ require("./config")(app);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-<<<<<<< HEAD
 const commentsRoutes = require("./routes/comment.routes");
-app.use("/comments", commentsRoutes)
-=======
+app.use("/comments", commentsRoutes);
 const eventRoutes = require("./routes/event.routes");
 app.use("/event", eventRoutes);
->>>>>>> 67cfbb140120304bf181b5406e3218543c6d7128
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
