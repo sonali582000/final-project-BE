e3b14536420cd2d07a332411c23f72eda30cada6
sonali582000 <rathodsonali393@gmail.com>
2024-01-31 17:10:18 +0100

    Added routes for user in auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -93,4 +93,15 @@ router.get("/verify", isAuthenticated, async (req, res) => {
   res.status(200).json(currentUser);
 });
 
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const oneUser = await User.findById(userId);
+    res.status(200).json(oneUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error while getting the User" });
+  }
+});
+
 module.exports = router;
